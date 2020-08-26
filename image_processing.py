6d2c96a9578d5f53b079b4b5747741db1bc326f0
Nadav Friedman <nadavf8@gmail.com>
2020-08-26 20:39:41 +0300

    Ball Tracking ± 200 pixels

--- image_processing.py
+++ image_processing.py
@@ -6,6 +6,8 @@ import numpy as np
 class Processor:
     def __init__(self):
         self.vid = cv2.VideoCapture("video.mp4")
+        self.downscale_factor = 50
+        self.ball_radius = 100
         self.frame = 0
 
     def __del__(self):
@@ -19,18 +21,37 @@ class Processor:
                 break
             frame = self.get_ball(frame)
             cv2.imshow('frame', frame)
-            # time.sleep(0.016)
+            time.sleep(0.016)
 
     def get_ball(self, frame):
-        small = cv2.resize(frame, (0, 0), fx=0.02, fy=0.02)
+        small = cv2.resize(frame, (0, 0), fx=1 / self.downscale_factor, fy=1 / self.downscale_factor)
+
         only_ball = np.apply_along_axis(self.threshold, 2, small)
-        big = cv2.resize(only_ball, (0, 0), fx=30, fy=30)
-        return big
+        ball_indexes = np.where(only_ball == True)
+
+        ball_x = np.average(ball_indexes[0])
+        ball_y = np.average(ball_indexes[1])
+
+        from_x, to_x, from_y, to_y = self.get_ball_range(ball_x, ball_y, frame.shape[:2])
+        ball_in_frame = frame[from_x:to_x, from_y:to_y]
+        return ball_in_frame
+
+    def get_ball_range(self, x, y, frame_size):
+        if str(x) == "nan":
+            return 0, 1, 0, 1
+        upscaled_x = np.floor(x * self.downscale_factor)
+        min_x = max(0, upscaled_x - self.ball_radius)
+        max_x = min(frame_size[0], upscaled_x + self.ball_radius)
+
+        upscaled_y = np.floor(y * self.downscale_factor)
+        min_y = max(0, upscaled_y - self.ball_radius)
+        max_y = min(frame_size[1], upscaled_y + self.ball_radius)
+        return int(min_x), int(max_x), int(min_y), int(max_y)
 
     @staticmethod
     def threshold(x):
         dist = abs(np.linalg.norm(np.array([125, 234, 255]) - x))
         if dist < 40:
-            return x
+            return True
         else:
-            return x//100
+            return False
