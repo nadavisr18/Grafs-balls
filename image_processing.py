c40857391717a1ab6cf397ddae293a7239062b06
Nadav Friedman <nadavf8@gmail.com>
2020-08-26 23:24:12 +0300

    Only Ball Tracking by Threshold

--- image_processing.py
+++ image_processing.py
@@ -21,7 +21,7 @@ class Processor:
                 break
             frame = self.get_ball(frame)
             cv2.imshow('frame', frame)
-            time.sleep(0.016)
+            time.sleep(0.1)
 
     def get_ball(self, frame):
         small = cv2.resize(frame, (0, 0), fx=1 / self.downscale_factor, fy=1 / self.downscale_factor)
@@ -32,21 +32,35 @@ class Processor:
         ball_x = np.average(ball_indexes[0])
         ball_y = np.average(ball_indexes[1])
 
-        from_x, to_x, from_y, to_y = self.get_ball_range(ball_x, ball_y, frame.shape[:2])
+        from_x, to_x, from_y, to_y = self.get_ball_range(ball_x, ball_y, frame)
         ball_in_frame = frame[from_x:to_x, from_y:to_y]
+        if 0 in ball_in_frame.shape:
+            ball_in_frame = self.handle_no_ball()
         return ball_in_frame
 
-    def get_ball_range(self, x, y, frame_size):
+    def get_ball_range(self, x, y, frame):
         if str(x) == "nan":
             return 0, 1, 0, 1
-        upscaled_x = np.floor(x * self.downscale_factor)
-        min_x = max(0, upscaled_x - self.ball_radius)
-        max_x = min(frame_size[0], upscaled_x + self.ball_radius)
+        upscaled_x = int(np.floor(x * self.downscale_factor))
+        upscaled_y = int(np.floor(y * self.downscale_factor))
+        min_x = self.walk(frame, upscaled_x, upscaled_y, [-1,0])[0] - 10
+        max_x = self.walk(frame, upscaled_x, upscaled_y, [ 1,0])[0] + 10
+        min_y = self.walk(frame, upscaled_x, upscaled_y, [0,-1])[1] - 10
+        max_y = self.walk(frame, upscaled_x, upscaled_y, [0, 1])[1] + 10
 
-        upscaled_y = np.floor(y * self.downscale_factor)
-        min_y = max(0, upscaled_y - self.ball_radius)
-        max_y = min(frame_size[1], upscaled_y + self.ball_radius)
         return int(min_x), int(max_x), int(min_y), int(max_y)
+
+    def walk(self, frame, x, y,direction):
+        while True:
+            x += direction[0]
+            y += direction[1]
+            pixel = frame[x,y]
+            if not self.threshold(pixel):
+                return [x,y]
+
+    @staticmethod
+    def handle_no_ball():
+        return np.ones((2,2,3))
 
     @staticmethod
     def threshold(x):
